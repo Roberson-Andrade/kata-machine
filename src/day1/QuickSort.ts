6ddcea0dfd7217df88434b50f8b08b9fce46ec3d
Roberson Andrade <roberson_souza11@hotmail.com>
2024-05-28 23:15:36 -0300

    add binary search, array list, two crystal balls, quick sort, maze, bubble sort, linear search and queue

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,37 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], low: number, high: number): void {
+    if (low >= high) {
+        return;
+    }
 
+    const partitionIdx = partition(arr, low, high);
+
+    qs(arr, low, partitionIdx - 1);
+    qs(arr, partitionIdx + 1, high);
+}
+
+function partition(arr: number[], low: number, high: number): number {
+    let pivot = arr[high];
+
+    let idx = low - 1;
+
+    for (let i = low; i < high; i++) {
+        if (arr[i] <= arr[high]) {
+            idx++;
+
+            const temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+        }
+    }
+
+    idx++;
+
+    arr[high] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jumpSpace = Math.floor(Math.sqrt(breaks.length));
 
+  let i = jumpSpace;
+
+  for(; i < breaks.length; i += jumpSpace) {
+    if(breaks[i]) {
+      break
+    }
+  }
+
+  i -= jumpSpace;
+
+  for(let j = 0; j < jumpSpace; j++, i++) {
+    if(breaks[i]) {
+      return i;
+    }
+  }
+
+  return -1;
+  
 }
