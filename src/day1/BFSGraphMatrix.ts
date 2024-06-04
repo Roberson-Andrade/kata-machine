cf941daf7465a082c2d6cb93e8fbbfd886dea0ef
Roberson Andrade <roberson_souza11@hotmail.com>
2024-06-04 08:51:53 -0300

    feat: add bfs and dfs in graph

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,37 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+  const seen = new Array(graph.length).fill(false);
+  const prev = new Array(graph.length).fill(-1);
 
+  const queue = [source];
+
+  seen[source] = true;
+
+  do {
+    const curr = queue.shift() as number;
+    
+    if(curr === needle) break;
+
+    const adjs = graph[curr];
+
+    for(let i = 0; i < adjs.length; i++) {
+      if(adjs[i] === 0 || seen[i]) continue;
+
+      seen[i] = true;
+      prev[i] = curr;
+
+      queue.push(i);
+    }
+  } while(queue.length);
+
+  if(prev[needle] === -1) return null
+
+  let curr = needle;
+  const out = [];
+
+  while(prev[curr] !== -1)  {
+    out.push(curr);
+    curr = prev[curr];
+  }
+
+  return [source].concat(out.reverse())
 }
--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,38 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function search(graph: WeightedAdjacencyList, curr: number, needle: number, path: number[], seen: boolean[]): boolean {
+  if(seen[curr]) {
+    return false;
+  }
 
+  seen[curr] = true;
+
+  if(graph[curr].length === 0) {
+    return false;
+  }
+
+  path.push(curr);
+
+  if(curr === needle) {
+    return true;
+  }
+
+  for(const edge of graph[curr]) {
+    if(search(graph, edge.to, needle, path, seen)) {
+      return true;
+    }
+  }
+
+  path.pop()
+
+  return false
 }
+
+export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+  const result: number[] = [];
+  const seen = new Array(graph.length).fill(false)
+
+  if(search(graph, source, needle, result, seen)) {
+    return result
+  }
+
+  return null;
+} 
